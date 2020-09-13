d47232106154934bf267f62d1b1cd501bdc65ba5
106820003 <t106820003@ntut.edu.tw>
2020-09-14 03:52:55 +0800

    新增拍照後點名介面(一)

--- project_app/app/src/main/java/com/example/myapplication/teacher_operation_which_mask_mode.java
+++ project_app/app/src/main/java/com/example/myapplication/teacher_operation_which_mask_mode.java
@@ -1,15 +1,31 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.content.Intent;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 public class teacher_operation_which_mask_mode extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_operation_which_mask_mode);
+
+        LinearLayout mainLinerLayout = (LinearLayout) this.findViewById(R.id.roll_call_layout);
+        for (int i = 0;i<50;i++){
+            TextView textview=new TextView(this);
+            textview.setTextSize(30);
+            textview.setWidth(250);   //設定寬度
+            textview.setHeight(70);
+            textview.setGravity(Gravity.CENTER);
+            textview.setText("你好！"+i);
+            mainLinerLayout.addView(textview);
+        }
     }
 
     public void onclick(View v) {
