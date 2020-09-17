022a27dc7fe36f08cca4131aa67a4d3da3936cf2
Kevin <kevin77688@gmail.com>
2020-09-17 14:02:17 +0800

    Add OpenCV library

---
+++ FaceRecognitionApp/app/src/main/java/org/ntut/faceRecognition/CameraCapture.java
@@ -0,0 +1,14 @@
+package org.ntut.faceRecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraCapture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_capture);
+    }
+}
