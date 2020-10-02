10ff6f0ffec443a50084abb391d7043956e81ef6
3135079602 <a0935210570602@gmail.com>
2020-10-03 04:27:48 +0800

    新增灰色手機

--- FaceRecognitionApp/app/src/main/java/org/ntut/faceRecognition/TeacherOperationTakePhoto.java
+++ FaceRecognitionApp/app/src/main/java/org/ntut/faceRecognition/TeacherOperationTakePhoto.java
@@ -38,18 +38,21 @@ public class TeacherOperationTakePhoto extends AppCompatActivity {
     }
 
     public void onclick(View v) {
-        Intent intent = new Intent();
-        intent.setAction("android.media.action.STILL_IMAGE_CAMERA");
-        switch(v.getId()){
+//        Intent intent = new Intent();
+//        intent.setAction("android.media.action.STILL_IMAGE_CAMERA");
+//        switch(v.getId()){
+//
+//
+//            case R.id.gray_return_button:
+//                intent.setClass(this , TeacherOperation.class);
+//                break;
+//        }
+//        startActivity(intent);
 
-            case R.id.button_return:
-                startActivity(intent);
-
-                break;
-
-            case R.id.gray_return_button:
-                intent.setClass(this , TeacherClass.class);
-                break;
-        }
+    }
+    public void _return(View v) {
+//        Intent intent = new Intent();
+//        intent.setClass(this , TeacherOperation.class);
+//        startActivity(intent);
     }
 }
