e46fe97268ddd027cb222ea45c1f43420f440dda
kevin77688 <kevin77688@gmail.com>
2020-10-19 14:45:14 +0800

    Update RetrofitClient with timeout

--- FaceRecognitionApp/app/src/main/java/org/ntut/faceRecognition/Retrofit/RetrofitClient.java
+++ FaceRecognitionApp/app/src/main/java/org/ntut/faceRecognition/Retrofit/RetrofitClient.java
@@ -1,5 +1,8 @@
 package org.ntut.faceRecognition.Retrofit;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -9,15 +12,24 @@ public class RetrofitClient {
     private static Retrofit instance = null;
 
     public static Retrofit getInstance() {
-        if (instance == null)
+        if (instance == null){
+
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(1, TimeUnit.MINUTES)
+                    .readTimeout(1, TimeUnit.MINUTES)
+                    .writeTimeout(1, TimeUnit.MINUTES)
+                    .build();
+
             instance = new Retrofit.Builder()
                     //remote
-//                    .baseUrl("http://nodejsserver.ddns.net:3000/")
+                    .baseUrl("http://nodejsserver.ddns.net:3000/")
                     //localhost
-                    .baseUrl("http://10.0.2.2:3000/")
+//                    .baseUrl("http://10.0.2.2:3000/")
+                    .client(okHttpClient)
                     .addConverterFactory(ScalarsConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build();
+        }
         return instance;
     }
 
