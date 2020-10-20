188145a5059af482db5f6c1565f16ab5cab2ddc3
Kevin Duan <kevin77688@gmail.com>
2020-10-21 02:54:52 +0800

    Python test

--- nodejs_mongo/detect.py
+++ nodejs_mongo/detect.py
@@ -5,6 +5,7 @@ import json
 from PIL import Image
 
 def cropImage(imageWaitForDetectionPath, userId):
+    return "false";
     originalImage = face_recognition.load_image_file(imageWaitForDetectionPath)
     face_locations = face_recognition.face_locations(originalImage)
     if (len(face_locations) != 1):
