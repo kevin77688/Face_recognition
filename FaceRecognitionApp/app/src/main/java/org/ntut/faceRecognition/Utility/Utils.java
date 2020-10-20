9104f2093dcce20a009224b52c2a15de4b22eded
Kevin Duan <kevin77688@gmail.com>
2020-10-21 00:28:56 +0800

    Check register and login format

--- FaceRecognitionApp/app/src/main/java/org/ntut/faceRecognition/Utility/Utils.java
+++ FaceRecognitionApp/app/src/main/java/org/ntut/faceRecognition/Utility/Utils.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utils {
     public static void showToast(String msg, Context ctx) {
         Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show();
@@ -18,5 +21,12 @@ public class Utils {
             }
         };
         return listener;
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
