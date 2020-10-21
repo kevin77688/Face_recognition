cb216ba6cdd09b1c4c28cdce98c10b39956a6eb8
kenny chen <KENNY8808@GMAIL.COM>
2020-10-21 15:22:14 +0800

    recognize multi face

--- nodejs_mongo/recognize.py
+++ nodejs_mongo/recognize.py
@@ -23,18 +23,32 @@ def detectFace(courseListPhoto_dir, detectPicture):
         known_faces.append(knownFace_encoding)
     # import test picture
     unknownFace = face_recognition.load_image_file(detectPicture)
-    unknownFace_encoding = face_recognition.face_encodings(unknownFace)[0]
-    
+    unknownFace_encodings = face_recognition.face_encodings(unknownFace)
+    resultStack = []
+    for unknownFace_encoding in unknownFace_encodings:
+        distance = 0.55
+        index = -1
+        localDistance = face_recognition.face_distance(known_faces, unknownFace_encoding)
+        for i in range(len(known_faces)):
+            if localDistance[i] < distance:
+                index = i
+                distance = localDistance[i]
+        if index != -1:
+            exist = False
+            for result in resultStack:
+                if fileNames[index][7:-4] == result:
+                    exist = True
+            if exist == False:
+                resultStack.append(fileNames[index][7:-4])
     # compare with dataset
     # distance = face_recognition.face_distance(known_faces, unknownFace_encoding)
     # for index in range(len(fileNames)):
         # print("{} : {}".format(fileNames[index], distance
-    results = face_recognition.compare_faces(known_faces, unknownFace_encoding, 0.55)
-    resultStack = []
-    # print result
-    for index in range(len(results)):
-        if results[index]:
-            resultStack.append(fileNames[index][7:-4])
+    # results = face_recognition.compare_faces(known_faces, unknownFace_encoding, 0.55)
+    # resultStack = []
+    # for index in range(len(results)):
+        # if results[index]:
+            # resultStack.append(fileNames[index][7:-4])
     return resultStack
 
 def main():
